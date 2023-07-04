0f71eb4afda4be733dd46ac4318eb2f8ff9b5460
luckymanlm0222 <luckyman.lm0222@gmail.com>
2023-07-03 23:43:11 -0400

    scroll is removed in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="landing text-white">
-      <div className="my-[18px] md:pt-24 mx-[48px] md:mx-[236px]">
+      <div className="my-[18px] md:pt-0 mx-[48px] md:mx-[236px]">
         <div className="md:hidden flex items-center justify-between">
           <img src="assets/images/logo.png" width={"71px"} />
           <img src="assets/images/ic_round-menu.png" width={"24px"} />
@@ -12,46 +12,46 @@ export default function Home() {
         <div className="grid md:grid-cols-2 gap-4">
           <div className="mt-[20px] md:mt-0">
             <div className="hidden md:block mb-4">
-              <img src="assets/images/logo.png" width={"186px"} />
+              <img src="assets/images/logo.png" width={"100px"} />
             </div>
-            <div className="text-[32px] break-words md:text-[54px] text-white font-bold">
+            <div className="text-[32px] break-words md:text-[30px] text-white font-bold">
               Imagine Design
               <br />
               Feel Good
             </div>
-            <div className="text-[12px] md:text-[22px] break-words text-white mb-[48px] mt-[24px]">
+            <div className="text-[12px] md:text-[16px] break-words text-white mb-[20px] mt-[5px]">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's
             </div>
-            <HomeBtn className="text-white text-[14px] md:text-[26px]">
+            <HomeBtn className="text-white text-[14px] md:text-[20px]">
               Get Started
             </HomeBtn>
           </div>
-          <div className="py-[32px] px-[23px] md:py-[64px] md:px-[56px] rounded-[10px] md:rounded-[20px] bg-[#0E1E2B] border border-[#FFF5F5] text-[12px] md:text-[22px] md:w-3/4 my-[29px] md:mt-10 md:ml-auto">
+          <div className="py-[32px] px-[23px] md:py-[30px] md:px-[35px] rounded-[10px] md:rounded-[20px] bg-[#0E1E2B] border border-[#FFF5F5] text-[12px] md:text-[16px] md:w-3/4 my-[29px] md:mt-0 md:ml-auto">
             <div>
               <div className="mb-[16px]">
                 Start your home interior design with rendro.ai
               </div>
-              <div className="mb-[18px]">
+              <div className="mb-[10px]">
                 <div className="mb-[8px]">Email:</div>
-                <input className="w-full h-[30px] md:h-[60px] rounded-[5px] md:rounded-[10px] bg-transparent border"></input>
+                <input className="w-full h-[30px] md:h-[40px] rounded-[5px] md:rounded-[10px] bg-transparent border"></input>
               </div>
               <div className="mb-[16px]">
                 <div className="mb-[8px]">Password:</div>
-                <input className="w-full h-[30px] md:h-[60px] rounded-[5px] md:rounded-[10px] bg-transparent border"></input>
+                <input className="w-full h-[30px] md:h-[40px] rounded-[5px] md:rounded-[10px] bg-transparent border"></input>
               </div>
               <HomeBtn className="text-black w-full">Sign In</HomeBtn>
             </div>
           </div>
         </div>
 
-        <div className="flex items-center justify-center py-16 relative">
+        <div className="flex items-center justify-center relative w-[60%] mx-auto">
           <img src="assets/images/home1.png" className="w-1/2" />
           <img src="assets/images/home2.png" className="w-1/2" />
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full bg-white  change-home-btn">
             <img
               src="assets/images/Code_light.png"
-              className="w-[15px] md:w-[50px] m-[8px] md:m-[25px]"
+              className="w-[15px] md:w-[30px] m-[8px] md:m-[15px]"
             />
           </div>
         </div>
